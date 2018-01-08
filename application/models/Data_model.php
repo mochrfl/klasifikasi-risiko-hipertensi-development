5efe3ed1bfafa8a8829a807b8bc0a0110003b9c3
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2018-01-08 22:05:02 +0700

    update

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -10,6 +10,7 @@ class Data_model extends CI_model {
 	{
     $this->db->select('*');
     $this->db->from('tb_data_training');
+    $this->db->where('is_deleted', 0);
     $query = $this->db->get();
 
     return $query->result();
